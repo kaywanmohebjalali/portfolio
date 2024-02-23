b9b4779688c1d6a21bf679093c57f22f06f7c487
kaywan mohebjalali <37100347+kaywanmohebjalali@users.noreply.github.com>
2024-02-24 00:26:18 +0330

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,15 @@
 
 const DataProjects = [
 
-    { id: 1, title: 'Hotel', description: 'hotel is best project', src: 'frontend-3.jpg', group: 'frontend',href:'https://hotel-wild.netlify.app' },
-    { id: 2, title: 'Map', description: 'map is best project', src: 'frontend-1.jpg', group: 'frontend',href:'#' },
-    { id: 3, title: 'quiz', description: 'note is best project', src: 'frontend-2.jpg', group: 'frontend',href:'#' },
-    { id: 4, title: 'portfolio', description: 'spa is best project', src: 'frontend-4.jpg', group: 'frontend',href:'#' },
+    { id: 1, title: 'Hotel', description: 'This hotel project is the most complete project I have done', src: 'hotel.jpg', group: 'frontend',href:'https://react-specialhotel.netlify.app' },
+    { id: 2, title: 'Map', description: 'The map project is very useful for people who travel to different parts of the world', src: 'map.jpg', group: 'frontend',href:'https://react-maptravel.netlify.app' },
+    { id: 3, title: 'quiz', description: 'This is a test questions project', src: 'quiz.jpg', group: 'frontend',href:'https://my-quiz-react-app.netlify.app' },
+    { id: 4, title: 'fastfood', description: 'This project is a simple fast food site', src: 'pizza.jpg', group: 'frontend',href:'https://fastfood-react-app.netlify.app' },
+    { id: 5, title: 'Travel notebook', description: 'This travel notebook project', src: 'travel.jpg', group: 'frontend',href:'https://my-travel-list-react-app.netlify.app' },
+   
+    { id: 6, title: 'calculator', description: 'This project is an almost advanced calculator', src: 'calculator.jpg', group: 'frontend',href:'https://calculator-javascript-app.netlify.app' },
+    { id: 7, title: 'atomicblog', description: 'This project is a small blog', src: 'blog.jpg', group: 'frontend',href:'https://atomicblog-react-app.netlify.app' },
+        
 ]
 
 export { DataProjects }
